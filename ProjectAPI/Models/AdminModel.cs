b7f643ad2689a4c8a6fda5e25db5d07b044e326c
AkashS27 <111862679+AkashS27@users.noreply.github.com>
2022-11-07 10:06:55 +0530

    Add files via upload

---
+++ ProjectAPI/Models/AdminModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace ProjectAPI.Models
+{
+    public class AdminModel
+    {
+        [Required(ErrorMessage ="Enter email")]
+        [Key]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage ="Enter Password and should be same as above password")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPass { get; set; }
+
+    }
+}
