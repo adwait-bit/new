af0a7fe1e3aa6297d37cc974ccd8f78aa83280c6
abdulwadoodawk <111861842+abdulwadoodawk@users.noreply.github.com>
2022-11-08 17:53:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes,Route,Link } from 'react-router-dom';
+import Home from './Common/Home';
+import AdminLogin from './Admin/AdminLogin';
+import AdminRegister from './Admin/AdminRegister';
+import UserLogin from './User/UserLogin';
+import UserRegister from './User/UserRegister';
+import ShowPolicy from './Policy/ShowPolicy';
+import RegisterPolicy from './Policy/RegisterPolicy';
+import SearchByType from './Policy/SearchByType';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+       <Route path='/' element={<Home/>}></Route>
+        <Route path='/Home' element={<Home/>}></Route>
+        <Route path='/ShowPolicy' element={<ShowPolicy/>}></Route> 
+       <Route path='/UserRegister' element={<UserRegister/>}></Route> 
+       <Route path='/AdminLogin' element={<AdminLogin/>}></Route>
+        {/* <Route path='/RegisterPolicy/:id' element={<RegisterPolicy/>}></Route> */}
+        <Route path='/RegisterPolicy' element={<RegisterPolicy/>}></Route>
+        <Route path='/UserLogin' element={<UserLogin/>}></Route>
+        <Route path='/AdminRegister' element={<AdminRegister/>}></Route>
+        <Route path='/SearchByType' element={<SearchByType/>}></Route>
+</Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Common/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../Common/Footer.css'
+export default class Footer extends Component {
+  render() {
+    return (
+      
+        <footer className='footer'>
+
+            <p>Copyright @ Hexaware.com. All rights resrved.</p>
+        </footer>
+
+      
+    )
+  }
+}
